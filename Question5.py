1c51690d4f29a6f625de56bbdabe61ea96cb69ba
Ziithe Ewen Hiwa <z.hiwa@alustudent.com>
2021-12-16 21:13:24 +0200

    Comment

--- Question5.py
+++ Question5.py
@@ -3,6 +3,7 @@
 import sys
 
 class Graph:
+    #initilise dictionary of the nodes and weight
     def __init__(self, n):
         self.size = n
         self.vertex = dict.fromkeys([n for n in range(n)])
@@ -13,6 +14,7 @@ class Graph:
         print(self.vertex)
 
     def add_edge(self, x, y, w):
+        #add the edges to graph
         if not y in self.vertex[x].keys() or self.vertex[x][y] > w:
             self.vertex[x][y] = w
             self.vertex[y][x] = w
@@ -20,13 +22,18 @@ class Graph:
     def shortestReach(self, graph, start):
         path = [-1] * graph.size
         path[start] = 0
+        #list of all visited edges
         visited = []
+        #initilise next edge from 0
         next_edge = {start:0}
         
         while bool(next_edge):
+            #check for edge with least weight
             node = min(next_edge, key=next_edge.get)
+            #delte already visited vertex
             del next_edge[node]
             
+            #check to add node to path
             for child in graph.vertex[node].keys():
                 if not child in visited:
                     temp_path = path[node] + graph.vertex[node][child]
