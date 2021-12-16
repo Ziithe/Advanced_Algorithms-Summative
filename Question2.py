60db2aa1ba24beaa359ce6c152e355b2679c1bbe
Ziithe Ewen Hiwa <z.hiwa@alustudent.com>
2021-12-16 15:25:41 +0200

    Q2

---
+++ Question2.py
@@ -0,0 +1,20 @@
+#Grading Rounding Process by Ziithe Ewen Hiwa
+
+def gradingStudents(n):
+    if 1<=n<=60:
+        for i in range(n):
+            grade = int(input("Please enter students grade: ").strip())
+            if grade > 100:
+                print("Grade cannot be more than 100")
+
+            elif 38<=grade<=100:
+                diff = grade % 5
+                if diff >= 3:
+                    grade = grade + (5 - diff)
+            print("Final grade is: ",grade)
+    else:
+        print("Cannot grade more than 60 students")
+            
+
+
+gradingStudents(int(input("Please enter the number of students you'd like to grade: ")))
